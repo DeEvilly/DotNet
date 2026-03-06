3a468d4cb44bc82c01dd7d2ffffdba35bb4ce2cb
Vlad <vlad.avramko123@gmail.com>
2026-03-06 17:11:46 +0200

    Lab01_Avramko

---
+++ Project_Avramko/DotNet_Lab01_Core/SysInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Lab01_Core
+{
+    public class SysInfo
+    {
+        public static void PrintCurrentOSVer()
+        {
+            Console.WriteLine($"Current OS version: {System.Environment.OSVersion}");
+        }
+
+        public static void PrintRAMUsage()
+        {
+            Console.WriteLine($"Total memory usage: {System.GC.GetTotalMemory(false)} bytes");
+        }
+    }
+}
